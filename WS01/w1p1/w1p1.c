0bf63048dba747538043b8e705d218cc3e80ec8e
renvsen <renv.seneca@gmail.com>
2023-01-23 15:23:43 -0500

    moving private to public

---
+++ WS01/w1p1/w1p1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+/*/////////////////////////////////////////////////////////////////////////
+                          Workshop - #1 (P1)
+Full Name  :    Rendell Velasco
+Student ID#:    140014218
+Email      :    rvelasco6@myseneca.ca
+Section    :    NDD
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+/////////////////////////////////////////////////////////////////////////*/
+
+int main(void)
+{
+    printf("Workshop 1 Part-1\n");
+    printf("=================\n");
+    printf("I'm displaying this using the 'printf' stdio\n");
+    printf("(standard input output) library function!\n\n");
+    printf("Dear teacher,\n\n");
+    printf("  I promise I will work hard from this day onward.\n");
+    printf("  I acknowledge that practice is extremely important,\n");
+    printf("  so I will do all workshops, quizzes, and assignments.\n\n");
+    printf("Sincerely,\n\n");
+    printf("Rendell Velasco\n");
+    printf("Student ID#: 140014218\n");
+
+    return 0;
+}
