0959360680b04940060a6a6f847fc26e5e587a2b
renvsen <renv.seneca@gmail.com>
2023-01-26 17:56:33 -0500

    Update w3p1.c

--- WS03/p1/w3p1.c
+++ WS03/p1/w3p1.c
@@ -63,9 +63,9 @@ int main(void)
     printf("1. Is product 1 taxable? -> %d\n\n", tax1 == 'Y');
     printf("2. Are products 2 and 3 both NOT taxable (N)? -> %d\n\n", tax2 && tax3 == 'N');
     printf("3. Is product 3 less than testValue ($%.2lf)? -> %d\n\n", testValue, price3 < testValue);
-    printf("4. Is the price of product 3 more than both product 1 and 2 combined> -> %d\n\n", price3 > (price1 + price2));
+    printf("4. Is the price of product 3 more than both product 1 and 2 combined? -> %d\n\n", price3 > (price1 + price2));
     printf("5. Is the price of product 1 equal to or more than the price difference\n");
-    printf("   of product 3 LESS product 2? ->  %d (price difference : $%.2lf)\n\n", price1 >= (price3 - price2), price3 - price2);
+    printf("   of product 3 LESS product 2? ->  %d (price difference: $%.2lf)\n\n", price1 >= (price3 - price2), price3 - price2);
     printf("6. Is the price of product 2 equal to or more than the average price? -> %d\n\n", price2 >= averagePrice);
     printf("7. Based on product ID, product 1 is unique -> %d\n\n", productID1 != productID2 && productID1 != productID3);
     printf("8. Based on product ID, product 2 is unique -> %d\n\n", productID2 != productID1 && productID2 != productID3);
