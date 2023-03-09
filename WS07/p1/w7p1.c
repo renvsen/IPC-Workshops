53a150a7f75872b206fa1e92c36d5b593908bd6f
renvsen <renv.seneca@gmail.com>
2023-03-09 01:03:43 -0500

    Finished v1.0

--- WS07/p1/w7p1.c
+++ WS07/p1/w7p1.c
@@ -1,9 +1,9 @@
 /*/////////////////////////////////////////////////////////////////////////
                           Workshop - #7 (P1)
-Full Name  : 
-Student ID#: 
-Email      : 
-Section    : 
+Full Name  :    Rendell Velasco 
+Student ID#:    140014218
+Email      :    rvelasco6@myseneca.ca
+Section    :    NDD
 
 Authenticity Declaration:
 I declare this submission is the result of my own work and has not been
@@ -15,7 +15,143 @@ piece of work is entirely of my own creation.
 
 #include <stdio.h>
 
+#define MIN_PATH_LENGTH 10
+#define MAX_PATH_LENGTH 70
+#define PATH_MULTIPLE 5
+#define MAX_LIVES 10
+
+struct PlayerInfo
+{
+    int nOfLives;
+    char symbol;
+    int treasuresFound;
+    int moves[MAX_PATH_LENGTH];
+};
+
+struct GameInfo
+{
+    int maxMoves;
+    int pathLength;
+    int bombs[MAX_PATH_LENGTH];
+    int treasures[MAX_PATH_LENGTH];
+};
+
 int main(void)
 {
+    struct PlayerInfo playerInfo = { 0, ' ', 0, {0} };
+    struct GameInfo gameInfo = { 0, 0, {0}, {0} };
+    int i, j, posn;
+
+    printf("================================\n");
+    printf("         Treasure Hunt!         \n");
+    printf("================================\n");
+    printf("\nPLAYER Configuration\n");
+    printf("--------------------\n");
+    printf("Enter a single character to represent the player: ");
+    scanf(" %c", &playerInfo.symbol);
+
+    do 
+    {
+        printf("Set the number of lives: ");
+        scanf("%d", &playerInfo.nOfLives);
+        
+        if (playerInfo.nOfLives < 1 || playerInfo.nOfLives > MAX_LIVES)
+        {
+            printf("     Must be between 1 and %d!\n", MAX_LIVES);
+        }
+
+    } while (playerInfo.nOfLives < 1 || playerInfo.nOfLives > MAX_LIVES);
+
+    printf("Player configuration set-up is complete\n");
+    printf("\nGAME Configuration\n");
+    printf("------------------\n");
+
+    do
+    {
+        printf("Set the path length (a multilpe of %d between %d-%d): ", PATH_MULTIPLE, MIN_PATH_LENGTH, MAX_PATH_LENGTH);
+        scanf("%d", &gameInfo.pathLength);
+
+        if ((gameInfo.pathLength % 5) != 0 || gameInfo.pathLength < MIN_PATH_LENGTH || gameInfo.pathLength > MAX_PATH_LENGTH)
+        {
+            printf("     Must be a multiple of %d and between %d-%d!!!\n", PATH_MULTIPLE, MIN_PATH_LENGTH, MAX_PATH_LENGTH);
+        }
+
+    } while ((gameInfo.pathLength % 5) != 0 || gameInfo.pathLength < MIN_PATH_LENGTH || gameInfo.pathLength > MAX_PATH_LENGTH);
+
+    do
+    {
+        printf("Set the limit for number of moves allowed: ");
+        scanf("%d", &gameInfo.maxMoves);
+
+        if (gameInfo.maxMoves < playerInfo.nOfLives || gameInfo.maxMoves >(0.75 * gameInfo.pathLength))
+        {
+            printf("     Value must be between %d and %d\n", playerInfo.nOfLives, (int)(0.75 * gameInfo.pathLength));
+        }
+
+    } while (gameInfo.maxMoves < playerInfo.nOfLives || gameInfo.maxMoves > (0.75 * gameInfo.pathLength));
+
+    printf("\nBOMB Placement\n");
+    printf("--------------\n");
+    printf("Enter the bomb positions in sets of %d where a value\n", PATH_MULTIPLE);
+    printf("of 1=BOMB, and 0=NO BOMB. Space-delimit your input.\n");
+    printf("(Example: 1 0 0 1 1) NOTE: there are %d to set!\n", gameInfo.pathLength);
+    
+    for ( posn = 0, j = 0; j < gameInfo.pathLength; j+=PATH_MULTIPLE)
+    {
+        printf("   Positions [%2d-%2d]: ", j + 1, j + 5);
+
+        for ( i = 0; i < PATH_MULTIPLE; i++, posn++)
+        {
+            scanf("%d", &gameInfo.bombs[posn]);
+        }
+    }
+
+    printf("BOMB placement set\n");
+    printf("\nTREASURE Placement\n");
+    printf("------------------\n");
+    printf("Enter the treasure placements in sets of %d where a value\n", PATH_MULTIPLE);
+    printf("of 1=TREASURE, and 0=NO TREASURE. Space-delimit your input.\n");
+    printf("(Example: 1 0 0 1 1) NOTE: there are %d to set!\n", gameInfo.pathLength);
+    
+    for (posn = 0, j = 0; j < gameInfo.pathLength; j += PATH_MULTIPLE)
+    {
+        printf("   Positions [%2d-%2d]: ", j + 1, j + 5);
+
+        for (i = 0; i < PATH_MULTIPLE; i++, posn++)
+        {
+            scanf("%d", &gameInfo.treasures[posn]);
+        }
+    }
+
+    printf("TREASURE placement set\n");
+    printf("\nGAME configuration set-up is complete...\n");
+    printf("\n------------------------------------\n");
+    printf("TREASURE HUNT Configuration Settings\n");
+    printf("------------------------------------\n");
+    printf("Player:\n");
+    printf("   %-11s: %c\n", "Symbol", playerInfo.symbol);
+    printf("   %-11s: %d\n", "Lives", playerInfo.nOfLives);
+    printf("   %-11s: [ready for gameplay]\n", "Treasure");
+    printf("   %-11s: [ready for gameplay]\n", "History");
+    printf("\nGame:\n");
+    printf("   %-11s: %d\n", "Path Length", gameInfo.pathLength);
+    printf("   %-11s: ", "Bombs");
+    
+    for ( i = 0; i < gameInfo.pathLength; i++)
+    {
+        printf("%d", gameInfo.bombs[i]);
+    }
+
+    printf("\n   %-11s: ", "Treasure");
+
+    for (i = 0; i < gameInfo.pathLength; i++)
+    {
+        printf("%d", gameInfo.treasures[i]);
+    }
+
+    printf("\n\n====================================\n");
+    printf("~ Get ready to play TREASURE HUNT! ~\n");
+    printf("====================================\n");
+
     return 0;
 }
